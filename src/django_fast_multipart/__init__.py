df739a2f9038ec8268aa20fed8fcb8ddd2c1d9ef
Pravin Kamble <iampbkamble@gmail.com>
2026-08-26 20:46:12 +0530

    Add Django parser selection middleware

--- src/django_fast_multipart/__init__.py
+++ src/django_fast_multipart/__init__.py
@@ -1,3 +1,4 @@
+from django_fast_multipart.middleware import FastMultipartMiddleware
 from django_fast_multipart.parser import RustMultiPartParser
 
-__all__ = ("RustMultiPartParser",)
+__all__ = ("FastMultipartMiddleware", "RustMultiPartParser")
